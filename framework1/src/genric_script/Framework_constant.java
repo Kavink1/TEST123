8d306a5575b54347fa53daeb0869b3ad78271d86
admin <admin@DESKTOP-A7U52L2>
2022-07-16 16:20:15 +0530

    SECOND RELEASE BUILD 2

---
+++ framework1/src/genric_script/Framework_constant.java
@@ -0,0 +1,12 @@
+package genric_script;
+
+public interface Framework_constant 
+{
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./softwares/geckodriver.exe";
+	
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./softwares/chromedriver.exe";
+	
+	String URL="https://facebook.com";
+}
